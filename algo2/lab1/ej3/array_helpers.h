b79dd7eb67b9a8b03bcb504da89e619955b5ab34
Adolfo <banchioadolfo@gmail.com>
2022-03-22 21:42:34 -0300

    a

--- algo2/lab1/ej3/array_helpers.h
+++ algo2/lab1/ej3/array_helpers.h
@@ -1,6 +1,6 @@
 
-#ifndef _ARRAYHELPERS
-#define _ARRAYHELPERS
+#ifndef ARRAY_HELPERS
+#define ARRAY_HELPERS
 
 unsigned int array_from_file(int a[], unsigned int max_size,const char *filepath);
 void array_dump(int a[], unsigned int length);
