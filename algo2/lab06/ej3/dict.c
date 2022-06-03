29061ebbb9b097956242f76e779e93acaa23f856
adolfo <banchioadolfo@gmail.com>
2022-06-03 19:07:36 -0300

    dict sin leaks

--- algo2/lab06/ej3/dict.c
+++ algo2/lab06/ej3/dict.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "dict.h"
 #include "key_value.h"
 
@@ -8,55 +9,171 @@ struct _node_t {
     key_t key;
     value_t value;
 };
-
+/*
 static bool invrep(dict_t d) {
-    /* needs implementation */
     return true;
 }
-
+*/
 dict_t dict_empty(void) {
     dict_t dict = NULL;
-    /* needs implementation */
     return dict;
 }
 
 dict_t dict_add(dict_t dict, key_t word, value_t def) {
-    /* needs implementation */
+    //assert(invrep(tree));
+    if (dict == NULL)
+    {
+        dict_t newe;
+        newe = malloc(sizeof(struct _node_t));
+        newe->key = word;
+        newe->value = def;
+        newe->right = NULL;
+        newe->left = NULL;
+        dict = newe;
+        return dict;
+    }
+    else if (key_eq(dict->key,word))
+    {
+        dict->value = def;
+    }else if (key_less(dict->key,word))
+    {
+        dict->right = dict_add(dict->right,word,def);
+    }else
+    {
+        dict->left = dict_add(dict->left,word,def);
+    }
+
+    //assert(invrep(tree));
     return dict;
 }
 
 value_t dict_search(dict_t dict, key_t word) {
     key_t def=NULL;
-    /* needs implementation */
-    return NULL;
+    if(dict_exists(dict,word))
+    {
+        if (key_eq(dict->key,word))
+        {
+            def = dict->value;
+        }else if (key_less(dict->key,word))
+        {
+            def = dict_search(dict->right,word);
+            //si la llave a busar es mayor que en la que estoy, voy a la derecha
+        }else
+        {
+            def = dict_search(dict->left,word);
+        }
+    }
+    return def;
 }
 
 bool dict_exists(dict_t dict, key_t word) {
-    /* needs implementation */
-    return false;
+    bool exists = false;
+    if (dict != NULL) //si el diccionario no es vacio nos fijamos si esta
+    {
+        if (key_eq(dict->key,word))
+        {
+            exists = true;
+        }else if (key_less(dict->key,word))
+        {
+            exists = dict_exists(dict->right,word);
+        }else
+        {
+            exists = dict_exists(dict->left,word);
+        }
+    }
+    return exists;
 }
 
 unsigned int dict_length(dict_t dict) {
-    /* needs implementation */
-    return 0u;
+    unsigned int length = 0u;
+    if (dict != NULL)
+    {
+        length = 1 + dict_length(dict->left) + dict_length(dict->right);
+    }
+    return length;
+}
+
+
+static dict_t minValueNode(dict_t dict){
+    dict_t current = dict ;
+    while (current != NULL && current->left !=NULL)
+    {
+        current = current->left;
+    }
+    return current;
 }
 
 dict_t dict_remove(dict_t dict, key_t word) {
-    /* needs implementation */
+    if (dict_exists(dict,word) || dict != NULL)
+    {
+        if (key_eq(dict->key,word))
+        {
+            if (dict->left == NULL)
+            {
+                dict_t temp = dict;
+                dict = dict->right;
+                free(temp);
+            }else if (dict->right == NULL)
+            {
+                dict_t temp = dict;
+                dict = dict->left;
+                temp->key = key_destroy(temp->key);
+                temp->value = value_destroy(temp->value);
+                free(temp);
+            }else{
+                dict_t temp = minValueNode(dict->right);
+                dict->key = temp->key;
+                dict->value = temp->value;
+                dict->right = dict_remove(dict->right,temp->key);
+            }
+        }else
+        {
+            if (key_less(dict->key,word))
+            {
+                dict->right = dict_remove(dict->right,word);
+            }else
+            {
+                dict->left = dict_remove(dict->left,word);
+            } 
+            
+        }
+    }
     return dict;
 }
 
 dict_t dict_remove_all(dict_t dict) {
-    /* needs implementation */
+    dict = dict_destroy(dict);
+    dict = dict_empty();
     return dict;
 }
 
 void dict_dump(dict_t dict, FILE *file) {
-    /* needs implementation */
+    if(dict != NULL)
+    {
+        dict_dump(dict->left,file);
+        key_dump(dict->key,file);
+        fprintf(file," : ");
+        value_dump(dict->value,file);
+        fprintf(file,"\n");
+        dict_dump(dict->right,file);
+    }
 }
 
 dict_t dict_destroy(dict_t dict) {
-    /* needs implementation */
+    //assert(invrep(dict));
+    if(dict != NULL)
+    {
+        dict->right = dict_destroy(dict->right);
+        dict->left = dict_destroy(dict->left);
+        dict->key = key_destroy(dict->key);
+        dict->value = value_destroy(dict->value);
+        free(dict);
+        dict = NULL;
+    }else
+    {
+        free(dict);
+        dict = NULL;
+    }
     return dict;
 }
 
--- algo2/lab06/ej3/string.c
+++ algo2/lab06/ej3/string.c
@@ -10,9 +10,9 @@ struct _s_string {
 
 string string_create(const char *word) {
     string str = NULL;
-    str = calloc(1, sizeof(struct _s_string));
-    str->length = /* needs implementation */;
-    str->content = calloc(/* needs implementation */, sizeof(char));
+    str = malloc(sizeof(struct _s_string));
+    str->length = strlen(word);
+    str->content = calloc(str->length +1, sizeof(char));
     str->content = strncpy(str->content, word, str->length + 1);
     return (str);
 }
@@ -22,8 +22,8 @@ unsigned int string_length(string str) {
 }
 
 bool string_less(const string str1, const string str2) {
-    /* needs implementation */
-    return false;
+    int cmp = strcmp(str1->content,str2->content);
+    return cmp;
 }
 
 bool string_eq(const string str1, const string str2) {
@@ -43,7 +43,7 @@ string string_destroy(string str) {
 }
 
 void string_dump(string str, FILE *file) {
-    fprintf(file, "%s", str->content);
+    fprintf(file, " %s ", str->content);
 }
 
 const char *string_ref(string str) {
