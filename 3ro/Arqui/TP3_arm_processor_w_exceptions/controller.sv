f9acc47359b594d86a448d122b7fa04a181e6e29
adolfo <adolfo.banchio@mi.unc.edu.ar>
2023-09-12 21:57:14 -0300

    arqui

--- 3ro/Arqui/TP3_arm_processor_w_exceptions/controller.sv
+++ 3ro/Arqui/TP3_arm_processor_w_exceptions/controller.sv
@@ -8,20 +8,34 @@ module controller(input logic [10:0] instr,
 				memtoReg, memRead, memWrite, ERet, Exc, ExtIAck);
 											
 	logic [1:0] AluOp_s;
-											
-	maindec 	decPpal 	(instr, 
-							reg2loc, 
-							AluSrc, 
+	logic NotAnInstr;										
+	maindec 	decPpal 	(instr,
+							reset,
+							reg2loc,  
 							memtoReg, 
 							regWrite, 
 							memRead,
 							memWrite, 
-							Branch, 
-							AluOp_s);	
+							Branch,
+							NotAnInstr,
+							ERet,
+							AluOp_s,
+							AluSrc);	
 					
 								
 	aludec 	decAlu 	(.funct(instr), 
 							.aluop(AluOp_s), 
 							.alucontrol(AluControl));
-			
+
+	always_comb begin
+    Exc <= ExtIRQ || NotAnInstr;
+    ExtIAck<= ExcAck && ExtIRQ;
+    
+    if (NotAnInstr)
+        EStatus <= 4'b0010;
+    else if (ExtIRQ)
+        EStatus <= 4'b0001;
+    else
+        EStatus <= 4'b0000;
+    end
 endmodule
