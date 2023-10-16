474dec7a1c7bc13ebb247cbacce71d6749180186
Adolfo <banchioadolfo@gmail.com>
2023-10-16 12:08:29 -0300

    andl

--- 3ro/Arqui/armv8 createAssembly-Template/main.s
+++ 3ro/Arqui/armv8 createAssembly-Template/main.s
@@ -1,10 +1,14 @@
 	.text
 	.org 0x0000
-	add x1, x2, x3 //x1 =5
-	stur x1, [x0, #0] //mem[0] = 5
-	sub x2, x15, x1 //x2 = 15-5 = 10
-	stur x2, [x0, #4] //mem[1] = 10
-	ldur x3, [x0, #0] //x3 = mem[0] = 5
-	add x4, x3, x2 //x4 = 5+10 = 15
-	ldur x4, [x0, #8] //mem[2] = 10
-
+	MOVZ x0, #0x0, LSL #0
+	MOVZ x1, #0x8, LSL #0
+	MOVZ x2, #0x10, LSL #0
+	MOVZ x3, #0x18, LSL #0
+	MOVZ x4, #0x1905, LSL #0
+	MOVZ x5, #0x1905, LSL #16
+	MOVZ x6, #0x1905, LSL #32
+	MOVZ x7, #0x1905, LSL #48
+	STUR x4, [x0, #0x0]
+	STUR x5, [x1, #0x0]
+	STUR x6, [x2, #0x0]
+	STUR x7, [x3, #0x0]
