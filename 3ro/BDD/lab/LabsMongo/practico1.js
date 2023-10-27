d40e211cbbc9933976aa1c9e465e9cdf5445c7f7
Adolfo <banchioadolfo@gmail.com>
2023-10-27 20:46:55 -0300

    bdd mongo

--- 3ro/BDD/lab/LabsMongo/practico1.js
+++ 3ro/BDD/lab/LabsMongo/practico1.js
@@ -20,8 +20,7 @@ names.forEach((name) => db.comments.insertOne({
     "date": new Date()
 })
 )
-names = ["Ale1", "ale2", "Ale3", "Ale4"]
-db.comments.find({ "name": "Ale1" });
+db.comments.find({ "name": {$in:["Ale1", "ale2", "Ale3", "Ale4"]} });
 
 
 /*
@@ -74,6 +73,71 @@ db.comments.find(
 ).limit(3);
 
 
+/*Listar el id del teatro (theaterId), estado (“location.address.state”), 
+ciudad (“location.address.city”), y coordenadas (“location.geo.coordinates”) 
+de los teatros que se encuentran en algunos de los estados "CA", "NY", "TX" 
+y el nombre de la ciudades comienza con una ‘F’. 
+Listar ordenados por estado y ciudad.*/
+db.theaters.find();
+db.theaters.find(
+    {
+        "location.address.state":{$in:["CA","NY","TX"]},
+        "location.address.city":{$regex:'^F'}
+    },
+    {
+        "theaterId":1,
+        "location.address.state":1,
+        "location.address.city":1,
+        "location.address.coordinates":1    
+    }
+).sort({
+    "location.address.state":1,
+    "location.address.city":1
+});
+
+/*Actualizar los valores de los campos texto (text) y fecha (date) del comentario cuyo 
+id es ObjectId("5b72236520a3277c015b3b73") a "mi mejor comentario" 
+y fecha actual respectivamente.*/
+db.comments.updateMany(
+    {
+        "_id":ObjectId("5b72236520a3277c015b3b73")   
+    },
+    {
+        $set:{
+                "text":"mi mejor comentario",
+                "date":ISODate()
+             }
+    });
+db.comments.find({"_id":ObjectId("5b72236520a3277c015b3b73")});
+    
+/*Actualizar el valor de la contraseña del usuario cuyo email es joel.macdonel@fakegmail.com a 
+"some password". La misma consulta debe poder insertar un 
+nuevo usuario en caso que el usuario no exista. 
+Ejecute la consulta dos veces. ¿Qué operación se realiza en cada caso?  
+(Hint: usar upserts). */
+db.users.updateOne(
+    {
+        "email":"joel.macdonel@fakegmail.com",
+    },
+    {
+        $set:{"password":"some password"}
+    },
+    {
+        upsert:true
+    });
+ 
+db.users.deleteOne({"email":"joel.macdonel@fakegmail.com"});
+    
+db.users.find({"email":"joel.macdonel@fakegmail.com"});
+/*Remover todos los comentarios realizados por el usuario 
+cuyo email es victor_patel@fakegmail.com durante el año 1980.*/
+db.comments.deleteMany(
+    {
+        "email":"victor_patel@fakegmail.com",
+        $expr:{$eq:[{$year: "$date"},1980]}
+    });
+    
+db.comments.find({"email":"victor_patel@fakegmail.com",$expr:{$eq:[{$year: "$date"},1980]}});
 
 
 
