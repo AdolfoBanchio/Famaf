6835ff23ce4a51682df1f2bb40ccbdcdada5d0e3
Adolfo Banchio <adolfo.banchio@mi.unc.edu.ar>
2025-11-13 18:12:22 -0300

    micro

---
+++ 5to/microcontroladores/notes.md
@@ -0,0 +1,6 @@
+# Sistemas embebidos y microcontroladores
+
+**Microcontrolador**: STM32F411x C/E, cortex M4
+**dev board**: STM32F407VGTX
+
+
---
+++ 5to/microcontroladores/stm32/light_tracker/Core/Inc/i2c_lcd_AIP31068.h
@@ -0,0 +1,27 @@
+#ifndef AIP31068_F4_H
+#define AIP31068_F4_H
+
+#include "stm32f7xx_hal.h"
+
+
+//------------------- LCD Initialization Service ----------------------
+void GROVE_LCD_Init(I2C_HandleTypeDef *hi2c);
+
+
+void GROVE_LCD_Clear(void);
+void GROVE_LCD_Home(void);
+void GROVE_LCD_SetCursor(uint8_t Col, uint8_t Row);
+void GROVE_LCD_WriteChar(char Ch);
+void GROVE_LCD_WriteString(char* Str);
+
+void GROVE_LCD_DisplayON(void);
+void GROVE_LCD_DisplayOFF(void);
+void GROVE_LCD_CursorON(void);
+void GROVE_LCD_CursorOFF(void);
+void GROVE_LCD_BlinkON(void);
+void GROVE_LCD_BlinkOFF(void);
+
+// Backlight.
+void GROVE_LCD_setBacklightRGB(uint8_t r, uint8_t g, uint8_t b);
+
+#endif
